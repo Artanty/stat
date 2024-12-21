f8ee4c6a60e17d60f12a582aba2347e38c8271fc
Artanty <thomasway7@gmail.com>
2024-12-21 23:13:47 +0300

    push back test;

--- back/app.js
+++ back/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pool = require('./core/db_connection');
 const cors = require('cors')
 const app = express();
+
 app.use(express.json());
 const bodyParser = require('body-parser');
 app.use(cors());

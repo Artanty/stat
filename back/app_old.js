43ab3312085de6beb2dedfc303ab1adfb183e89f
Artanty <thomasway7@gmail.com>
2024-12-23 02:16:34 +0300

    triga

--- back/app_old.js
+++ back/app_old.js
@@ -45,6 +45,7 @@ app.get('/add-event', async (req, res) => {
   }
 });
 
+
 app.post('/get-events', async (req, res) => {
   const { id, projectId, namespace, stage, isError, date } = req.body;
   let query = 'SELECT * FROM events';

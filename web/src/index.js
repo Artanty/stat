e88c4dff9c778e66282af6a77f3e7fb12bec7bc8
Artanty <thomasway7@gmail.com>
2025-03-26 14:22:49 +0300

    trigger deploy;

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,10 @@
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { App } from "./App";
 import GetUpdates from "./GetUpdates";
+import './style.css';
 import '/node_modules/react-grid-layout/css/styles.css';
 import '/node_modules/react-resizable/css/styles.css';
-import './style.css';
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
 
 const root = document.getElementById("root");
 
